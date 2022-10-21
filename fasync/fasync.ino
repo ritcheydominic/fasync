4511bf67f5cb1206a5e9570809d90c28d94050d6
Dominic Ritchey <dominic@ritchey.family>
2022-10-20 22:10:59 -0400

    Add System Sensor sync
    Waiting on strobes to test

--- fasync/fasync.ino
+++ fasync/fasync.ino
@@ -1,9 +1,35 @@
+#define SYSTEM_SENSOR 1
+#define SYNC_PIN      9
+
+const int syncType = SYSTEM_SENSOR;
+bool initSyncPulse;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(SYNC_PIN, OUTPUT);
+  initSyncPulse = true;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (initSyncPulse) {
+    if (syncType == SYSTEM_SENSOR) {
+      digitalWrite(SYNC_PIN, HIGH);
+      delay(1980);
+      digitalWrite(SYNC_PIN, LOW);
+      delay(20);
+      digitalWrite(SYNC_PIN, HIGH);
+      delay(20);
+      digitalWrite(SYNC_PIN, LOW);
+      delay(20);
+    }
+    initSyncPulse = false;
+  } else {
+    if (syncType == SYSTEM_SENSOR) {
+      digitalWrite(SYNC_PIN, HIGH);
+      delay(980);
+      digitalWrite(SYNC_PIN, LOW);
+      delay(20);
+    }
+  }
 }
